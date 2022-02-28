ec70f8706654ef152556bd446b45be577641c3ae
prokittikun <76927239+prokittikun@users.noreply.github.com>
2022-02-28 21:57:01 +0700

    Push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Navbar from "./components/header/Navbar";
 
 function App() {
+  const [TodoList, setTodoList] = useState([]);
+
+  const [Title, setTitle] = useState("");
+  const Submit = (e) => {
+    e.preventDefault();
+    if (Title.length === 0) return;
+    setTodoList([...TodoList, Title]);
+    setTitle("");
+  };
+  useEffect(() => {
+    console.log("TodoList ->", TodoList);
+  }, [TodoList]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar title="Todo list" />
+      <form onSubmit={Submit} className="container">
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Test</label>
+          <input
+            value={Title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+      <ul>
+        {TodoList.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
